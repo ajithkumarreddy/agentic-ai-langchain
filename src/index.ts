696e6bdf5e32745929580429edb8c60aa63fb758
Ajith Kumar <ajithkumarreddy123@gmail.com>
2026-09-21 18:45:12 +0530

    feat: added summarization middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 
-import { createAgent, ResponseFormat } from "langchain";
+import { createAgent, CreateAgentParams, ResponseFormat } from "langchain";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { StructuredTool } from "@langchain/core/tools";
 
@@ -10,14 +10,21 @@ export const model = new ChatGoogleGenerativeAI({
   maxRetries: 2,
 });
 
+type AgentOptions = Omit<
+  Parameters<typeof createAgent>[0],
+  "model" | "tools" | "responseFormat"
+>;
+
 const createLLMAgent = (
   tools: StructuredTool[] = [],
   responseFormat?: ResponseFormat | any,
+  options: AgentOptions = {},
 ) => {
   return createAgent({
     model,
     tools,
     responseFormat,
+    ...options,
   });
 };
 
